8011e97c42e331f981212c0fb0383c66b0ce3725
Aadi0706 <97503774+Aadi0706@users.noreply.github.com>
2022-06-09 07:55:50 +0530

    intial settings done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import React from "react";
+// import task from "./data/tasks.json";
+// import Task from "./components/TaskApp";
+import {AddTask} from "./components/AddTask";
+
 function App() {
-  return <div>{/* Code Here */}</div>;
+  return <div>{/* Code Here */}
+  I am aditya Choubey
+{/* 
+<TaskApp task={task}/> */}
+<AddTask/>
+
+  </div>;
 }
 
 export default App;
--- src/components/AddTask/AddTask.jsx
+++ src/components/AddTask/AddTask.jsx
@@ -1,12 +1,27 @@
 import React from "react";
 import styles from "./addTask.module.css";
+import { useState } from "react";
 
-const AddTask = () => {
-  // NOTE: do not delete `data-testid` key value pair
+ const AddTask = (handleAddTodo) => {
+  // NOTE: do not delete `data-testid` key value pair.
+const [todos,setTodos] =useState("");
+
+const handleinput = (e)=>{
+  setTodos({...todos,
+    text:e.target.value
+  });
+}
+  const handleSubmit = ()=>{
+    handleAddTodo(todos);
+
+  }
+
   return (
     <div className={styles.todoForm}>
-      <input data-testid="add-task-input" type="text" />
-      <button data-testid="add-task-button"></button>
+
+      <input data-testid="add-task-input" type="text" value={todos} onChange={handleinput} />
+
+      <button data-testid="add-task-button"  onSubmit={handleSubmit}></button>
     </div>
   );
 };
